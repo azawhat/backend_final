c80f28df343cee7696083d459a7fc55c2bac9f95
azawhat <azamat.erezhepbayev@gmail.com>
2024-02-23 00:59:08 +0600

    Status update

--- routes/server-payment.js
+++ routes/server-payment.js
@@ -28,6 +28,7 @@ router.get('/paypal', authenticateToken, async (req, res) => {
         res.status(500).render('error', { message: 'Internal Server Error' });
     }
 });
+
 router.get('/success', authenticateToken, async (req, res) => {
     try {
         const userId = req.user.userId;
@@ -36,18 +37,12 @@ router.get('/success', authenticateToken, async (req, res) => {
         for (const item of cartItems.rows) {
             await pool.query('UPDATE cart SET payment_status = true WHERE course_id = $1', [item.course_id]);
         }
-
-        const paidCourses = await pool.query('SELECT courses.id AS course_id, courses.title, courses.price FROM cart JOIN courses ON cart.course_id = courses.id WHERE cart.user_id = $1 AND cart.payment_status = true', [userId]);
-
-        await pool.query('DELETE FROM cart WHERE user_id = $1', [userId]);
-
-        res.render('payment-success', { paidCourses: paidCourses.rows });
+        res.render('payment-success');
     } catch (error) {
         console.error('Error processing payment success:', error);
         res.status(500).render('error', { message: 'Internal Server Error' });
     }
 });
-
 
 router.get('/cancel', (req, res) => {
     res.render('payment-cancel'); 
