046243fc79e119fa35b26288fa7750c03ec566a9
azawhat <azamat.erezhepbayev@gmail.com>
2024-02-22 19:31:34 +0600

    Changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const cartRouter = require('./routes/server-cart');
 const paymentRouter = require('./routes/server-payment');
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT environment variable provided by Render, or default to 3000
-
+require('dotenv').config()
 
 let jwtToken = '';
 
